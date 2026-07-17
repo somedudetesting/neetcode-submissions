512df0657590fbf1aeddca820675f9a7b8ca39bf
somedudetesting <arnav2k5us@gmail.com>
2026-07-17 14:40:03 +0530

    Add: guess-number-higher-or-lower - submission-2

---
+++ Data Structures & Algorithms/guess-number-higher-or-lower/submission-2.cpp
@@ -0,0 +1,27 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int l = 1, r = n;
+        do {
+            int mid = l + (r - l) / 2;
+            int pred = guess(mid);
+            if (pred == -1) {
+                r = mid - 1;
+            } else if (pred == 1) {
+                l = mid + 1;
+            } else {
+                return mid;
+            }
+        } while (l <= r);
+        return 0;
+    }
+};
