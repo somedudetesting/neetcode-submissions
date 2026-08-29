d7954890fea501757107148cf04c82c33eb0560e
somedudetesting <arnav2k5us@gmail.com>
2026-08-29 10:45:27 +0530

    Add: arranging-coins - submission-1

---
+++ Data Structures & Algorithms/arranging-coins/submission-1.cpp
@@ -0,0 +1,26 @@
+#define ll long long
+
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        /* since each row consists of i coins where 0 < i <= n; 
+        it forms a natural number sequence 
+        we can use the formula (k * (k + 1)) / 2 to get the coins for a mid value
+        see if it is within n so no incomplete row, or exceeds n, so there will be incomplete row*/
+
+        ll l = 1, r = (n / 2) + 1, res = 0;
+
+        while (l <= r) {
+            ll mid = l + (r - l) / 2;
+            ll coins = (mid * (mid + 1)) / 2;
+            if (coins > n) {
+                r = mid - 1;
+            } else {
+                res = max(res, mid);
+                l = mid + 1;
+            }
+        }
+
+        return res;
+    }
+};
